6e19688690c7fcc31dcc653d3c144b1eaa9f6051
blueline1318 <blueline1318@gmail.com>
2022-07-05 11:17:17 +0900

    initial commit

---
+++ app.js
@@ -0,0 +1,34 @@
+require("dotenv").config();
+
+const createError = require("http-errors");
+const express = require("express");
+const http = require("http");
+
+const app = express();
+
+const port = process.env.PORT || 8080;
+const server = http.createServer(app);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+app.use(function (err, req, res, next) {
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  res.status(err.status || 500);
+  res.json({
+    status: res.status,
+    message: "error",
+  });
+});
+
+server.listen(port);
+server.on("error", (error) => console.error(error));
+server.on("listening", () => console.log(`listening on port ${port}`));
+
+module.exports = app;
